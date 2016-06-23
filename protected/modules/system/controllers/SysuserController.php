a31718ccc9537f72ee4d001334bda40fb927feb9
Demon Zhang <demon.zhang@samesamechina.com>
2016-06-23 16:15:29 +0800

    no message

---
+++ protected/modules/system/controllers/SysuserController.php
@@ -0,0 +1,47 @@
+<?php
+class SysuserController extends SystemController
+{
+
+	public function actionIndex()
+	{
+		$permissions = new Permissions();
+		$permissionsJson = $permissions->listForcombobox();
+		$this->render('index',array('permissions'=>$permissionsJson));
+	}
+
+	public function actionList()
+	{
+		if(isset($_POST)){
+			$sysuser = new Sysuser();
+			$sysuserJson = $sysuser->listForEdit($_POST);
+			echo $sysuserJson;
+			Yii::app()->end();
+		}
+		echo json_encode(array('code'=>'3','msg'=>'参数错误'));
+		Yii::app()->end();
+	}
+
+	public function actionAdd()
+	{
+		if(isset($_POST)){
+			$sysuser = new Sysuser();
+			$sysuserJson = $sysuser->add($_POST);
+			echo $sysuserJson;
+			Yii::app()->end();
+		}
+		echo json_encode(array('code'=>'3','msg'=>'参数错误'));
+		Yii::app()->end();
+	}
+
+	public function actionUpdate()
+	{
+		if(isset($_POST)){
+			$sysuser = new Sysuser();
+			$sysuserJson = $sysuser->update($_POST);
+			echo $sysuserJson;
+			Yii::app()->end();
+		}
+		echo json_encode(array('code'=>'3','msg'=>'参数错误'));
+		Yii::app()->end();
+	}
+}
---
+++ protected/views/index.php
@@ -0,0 +1,65 @@
+
+
+<div id="loading">
+	<div class="loading-logo">
+		<div class="loading-mask"></div>
+	</div>
+</div>
+
+<div class="homepage">
+	<div class="qrcode">
+		<a href="javascript:;" class="close">
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/cw/images/close.png" />
+		</a>
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/cw/images/qrcode.png" class="qrcodeImg" />
+	</div>
+
+	<div href="javascript:;" class="logo">
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/cw/images/logo.png" width="100%" />
+	</div>
+
+	<div class="slogan">
+		<div class="hands_animate">
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/cw/images/hands.png" width="100%" />
+		</div>
+		<a href="javascript:;">
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/cw/images/slogan.png" width="100%" />
+		</a>
+	</div>
+	
+	<img src="<?php echo Yii::app()->request->baseUrl; ?>/cw/images/index.jpg" id="index_bg" width="100%" />
+</div>
+
+
+
+<div class="burger">
+	<div class="burger_con">
+		<span class="top" style="width: 100%;"></span>
+		<span class="center" style="width: 100%;"></span>
+		<span class="bottom" style="width: 100%;"></span>
+	</div>
+</div>
+
+<div class="menu" id="menu">
+	<ul></ul>
+</div>
+
+<div id="mask"></div>
+
+<span class="arr"></span>
+
+
+
+
+<div id="fullpage"></div>
+
+
+
+
+
+
+
+
+
+
+
