e3b39aa28035e72ec76fed2436addf3cf48a6fa9
evelyn_zhang <1054982653@qq.com>
2016-07-06 20:09:26 +0800

    qrcode

--- app/js/qrcode.js
+++ app/js/qrcode.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
     $('.qrshow').on('touchstart', function(){
-        var pophtml = $(this).find('.qrcode').html()+'<div class="qrdes">扫描或长按二维码<br>关注法国娇兰官方微博</div>';
-        Common.qrcodeBox.add(pophtml);
+        if($(this).hasClass('qrcode-1')){
+            var pophtml = $(this).find('.qrcode').html()+'<div class="qrdes">扫描或长按二维码<br>关注法国娇兰官方微博</div>';
+            Common.qrcodeBox.add(pophtml);
+        }else{
+            var pophtml = $(this).find('.qrcode').html()+'<div class="qrdes">扫描或长按二维码<br>前往法国娇兰官方商城</div>';
+            Common.qrcodeBox.add(pophtml);
+        }
+
     });
 
     $('body').on('touchstart','.btn-close', function(){
