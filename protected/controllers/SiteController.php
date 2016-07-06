7cbb1702e2d53b5b5f3e52c37f6a5d0b2e63a91e
Demon Zhang <demon.zhang@samesamechina.com>
2016-07-06 11:55:13 +0800

    no message

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -11,7 +11,13 @@ class SiteController extends Controller
 	{
 		$this->render('index');
 	}
-
+	
+	public function actionStore($id)
+	{
+		$sql = "select * from same_store where id = ".intval($id);
+		$store = Yii::app()->db->createCommand($sql)->queryRow();
+		$this->renderPartial('store', array('store' => $store));
+	}
 	/**
 	 * This is the action to handle external exceptions.
 	 */
