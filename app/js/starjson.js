8ab59a9fa2dee39ae716875d02b93e42b6ae5743
evelyn_zhang <1054982653@qq.com>
2016-07-12 16:59:43 +0800

    change the copy

--- app/js/starjson.js
+++ app/js/starjson.js
@@ -1,9 +1,9 @@
 var starProducts = [
   {
     id:1,
-    productname:'水晶蜜蜂',
-    productdes:'水晶蜜蜂雕刻品采用巴卡拉水晶瓶制作带翼轮廓，瓶身如同钻石刻面，闪耀无限光辉，实乃杰作。',
-    productintro:'这款水晶蜜蜂雕刻品采用巴卡拉水晶瓶制作带翼轮廓，瓶身如同钻石刻面，闪耀无限光辉，实乃杰作。无论是丝质barbichage、水晶涂抹棒还是柔软的皮革珠宝盒，均呈现了顶级法式奢华、美丽和完美。',
+    productname:'翼之蜂华',
+    productdes:'用巴卡拉水晶制成的翼之蜂华——一件真正意义上的杰作。 ',
+    productintro:'这款带翼轮廓的香水宝石——翼之蜂华，采用巴卡拉水晶制作而成。瓶身如同钻石表面，闪耀无限光芒，成为动人杰作。',
     productimgsrc:'/app/images/starproduct/product-1.jpg',
     thumbnail:'/app/images/starproduct/product-1.png',
     price:'17 000,00€',
@@ -13,7 +13,7 @@ var starProducts = [
     id:2,
     productname:'香水腕表',
     productdes:'一款用白金和珠宝制成的华美腕表，而它最珍贵的地方莫过于腕表中央盛有几滴曼妙香水。在链盒上轻微加压，便会释放出迷人的香味。 ',
-    productintro:'法国娇兰将经典香氛与精湛制表技艺融合，创造了别具一格的奢华腕表。在18克拉白金和大量黑白钻石拥簇中，如同银河系中的散发着点滴韶光的恒星星座沉浸其中，令时光的流逝光感闪耀，成为制表经典。如果在链盒上轻微加压，会有香氛隔空溢出，将时间附上浪漫的香气。',
+    productintro:'法国娇兰将经典香氛与精湛制表技艺融合，创造了别具一格的奢华腕表。在18克拉白金和大量黑白钻石拥簇中，如同银河系中散发着点滴韶光的恒星星座，令时光的流逝光感闪耀，成为制表经典。如果在链盒上轻微加压，法国娇兰“午夜飞行”香氛将会隔空溢出，将时间附上浪漫的香气。 ',
     productimgsrc:'/app/images/starproduct/product-7.jpg',
     thumbnail:'/app/images/starproduct/product-7.jpg',
     price:'价格店洽',
@@ -32,7 +32,7 @@ var starProducts = [
     class:'withpadding fan',
     productname:'芳香折扇',
     productdes:'两款美丽的折扇，“金色折扇”可作为日常搭配，“新娘捧花折扇”可点缀你最特别的日子。 ',
-    productintro:'<p>自上古以来，折扇一直是体现优雅和舒适的配件。法国娇兰和折扇商Duvelleroy制作了两款独家折扇，希望借此复兴这一精致的时尚传统及其共有的历史。精湛的手工技艺致敬法国的上等折扇制作工艺。法国娇兰将芬芳香气赋予优雅折扇，展开栩栩如生的华丽篇章。</p><h3>“新娘捧花”折扇</h3><p>极度轻盈丰满，点缀白色羽毛和刺绣珍珠亮片，为新娘一生中最特别的一天增添馥郁芬芳。</p><div class="price-wrap"><span class="product-price"><span>7600,00 €</span></span></div><div><img src="/app/images/starproduct/product-4-6.jpg" alt=""></div><h3>金色折扇</h3><p>金色折扇, 将娇兰挚爱的黄金材质放在首要地位，展开后如同孔雀般奢华，折扇散发迷人香味。</p><div class="price-wrap"><span class="product-price"><span>5100,00 €</span></span></div>',
+    productintro:'<p>自上古以来，折扇一直是体现优雅和舒适的配件。法国娇兰和折扇商Duvelleroy制作了两款独家折扇，希望借此复兴这一精致的时尚传统及其共有的历史。精湛的手工技艺致敬法国的上等折扇制作工艺。法国娇兰将芬芳香气赋予优雅折扇，展开栩栩如生的华丽篇章。</p><h3>“执子之手”折扇 </h3><p>极度轻盈丰满，点缀白色羽毛和刺绣珍珠亮片，为新娘一生中最特别的一天增添馥郁芬芳。</p><div class="price-wrap"><span class="product-price"><span>7600,00 €</span></span></div><div><img src="/app/images/starproduct/product-4-6.jpg" alt=""></div><h3>金色折扇</h3><p>以黄金装饰黑色丝缎页扇，乌木质地的扇框融入了蜂蜜和琥珀色调的秸秆镶嵌工艺。展开后如同孔雀般奢华，散发迷人香味。</p><div class="price-wrap"><span class="product-price"><span>5100,00 €</span></span></div>',
     productimgsrc:'/app/images/starproduct/product-4-2.jpg',
     thumbnail:'/app/images/starproduct/product-4.jpg',
     //price:'7600,00 €',
@@ -42,7 +42,7 @@ var starProducts = [
     id:5,
     productname:'定制香氛',
     productdes:'法国娇兰特调的定制香水，为您量身打造芳香故事。',
-    productintro:'在最初的时候，法国娇兰的创始人只按照客人需求为他们定制专属香水。现在，这个传统依然如故。法国娇兰在香味和个性之间发现了奇妙的平衡点。作为少数人的特享，法国娇兰调香师定制每款香氛都如同定制成衣般拥有精细而又漫长的过程。法国娇兰香水部门的总监研习了不同个性人们的特质，让定制香氛能够极其准确地适应个人。定制香氛如同无形的标签，使得拥有它的人成为法国娇兰品牌最特别的客人，在香水的芳香中，带着宾客进入法国娇兰的历史世界。',
+    productintro:'你是否在寻找一款专属自己的香水？      在最初的时候，法国娇兰创始人只按照客人需求为他们定制专属香水。现在，这个传统依然留传。法国娇兰在香味和个性化之间发现了奇妙的平衡点。作为少数人的尊享，法国娇兰调香师定制每款香氛都如同定制成衣般拥有精细而又漫长的过程。                                法国娇兰香水部门的总监研习了不同个性的特质，让定制香氛能够极其准确地适应个人。定制香氛如同无形的标签，使得拥有它的人成为法国娇兰品牌最特别的客人，在香水的芳香中，带着宾客进入法国娇兰的历史世界。',
     productimgsrc:'/app/images/starproduct/big-product-5.jpg',
     thumbnail:'/app/images/starproduct/product-5.jpg'
   },
@@ -67,7 +67,7 @@ var starProducts = [
     id:8,
     productname:'JonOne系列',
     productdes:'著名街头艺术偶像JonOne与法国娇兰携手，让奢华感和街头文化产生了奇妙碰撞，JonOne亲自装饰了蜜蜂瓶，呈现出非同一般的别致气质。',
-    productintro:'法国娇兰与街头艺术家JonOne携手，重塑经典蜜蜂瓶，以彩绘重新演绎现代时尚。三个色彩的宇宙，承载着各自的香味星云。属于“小黑裙”的粉色，“一千零一夜”的蓝色，“野蛮玫瑰”的橘色在瓶身的边界漫延。这些“JonOne”蜜蜂瓶由JonOne在巴黎的工作室中亲自绘色，在娇兰之家限量贩售。',
+    productintro:'法国娇兰与美国街头艺术家JonOne携手，重塑经典蜂姿瓶，以彩绘重新演绎现代时尚。三个色彩的宇宙，承载着各自的香味星云。属于“小黑裙”的粉色，“一千零一夜”的蓝色，“野蛮玫瑰”的橘色在瓶身蔓延。这些“JonOne”蜂姿瓶由JonOne在巴黎的工作室中亲自绘色，在娇兰之家限量发售。',
     productimgsrc:'/app/images/starproduct/product-8.jpg',
     thumbnail:'/app/images/starproduct/product-8.jpg',
     price:'9000,00 €'
