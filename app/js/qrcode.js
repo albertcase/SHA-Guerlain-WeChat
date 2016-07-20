4281043bcf7e88b22b58102f2ca7f0257df621f9
evelyn_zhang <1054982653@qq.com>
2016-07-20 16:04:38 +0800

    add qrcode

--- app/js/qrcode.js
+++ app/js/qrcode.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
-    $('.qrshow').on('touchstart', function(){
-        if($(this).hasClass('qrcode-1')){
-            var pophtml = '<div class="myimg">'+$(this).find('.qrcode').html()+'</div><div class="qrdes">扫描或长按二维码<br>关注法国娇兰官方微博</div>';
-            Common.qrcodeBox.add(pophtml);
-        }else{
-            var pophtml = '<div class="myimg">'+$(this).find('.qrcode').html()+'</div><div class="qrdes">扫描或长按二维码<br>前往法国娇兰官方商城</div>';
-            Common.qrcodeBox.add(pophtml);
+
+    var qrJson = [
+        {
+            img:'/app/images/qrcode-3.png',
+            des:'扫描或长按二维码<br>前往法国娇兰官方公众号'
+        },{
+            img:'/app/images/qrcode-2.png',
+            des:'扫描或长按二维码<br>前往法国娇兰官方商城'
+        },{
+            img:'/app/images/qrcode-1.png',
+            des:'扫描或长按二维码<br>关注法国娇兰官方微博'
         }
+    ];
+    $('.qrshow .qrcode').on('touchstart', function(){
+
+        var theIndex = $(this).parent().index();
+        var pophtml = '<div class="myimg"><img src="'+qrJson[theIndex].img+'"></div><div class="qrdes">'+qrJson[theIndex].des+'</div>';
+        Common.qrcodeBox.add(pophtml);
 
     });
 
