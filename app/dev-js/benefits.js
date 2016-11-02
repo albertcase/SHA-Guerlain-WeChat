eeb81cb8f23f50bda03fe0df9d53ca75e9022105
evelyn_zhang <1054982653@qq.com>
2016-11-02 16:42:29 +0800

    benefits and form page

--- app/dev-js/benefits.js
+++ app/dev-js/benefits.js
@@ -4,6 +4,18 @@ $(function(){
         window.location.href = "form-getbenefits.html";
     });
 
+    //switch-checkbox
+    $('.switch-checkbox').on('touchstart', function(){
+        $(this).toggleClass('selected');
+    });
+
+    //show-terms
+    $('.show-terms').on('touchstart', function(){
+        var popContent = '<h3 class="title">条款及条件</h3><div class="popcontent"><p>1.	本次活动自10月3日起，限前1000名关注者领取活动赠品，数量有限，赠完为止。</p><p>2.	本次活动只限中国大陆地区参与，敬请谅解。</p><p>3.	在符合法律相关规定的情况下，您必须同意活动主办方获得您的姓名，头像与个人信息，主办方并不需要为此再支付额外费用。</p><p>4.	本活动的最终解释权归法国娇兰香榭丽舍大道68号娇兰之家所有。</p></div>';
+
+        Common.popupBox.add(popContent);
+    });
+
     //form validate
     function formValidate(){
         var validate = true,
@@ -31,6 +43,11 @@ $(function(){
         }else{
             Common.removeClass(inputAddress.parentElement,'error');
         }
+
+        if(!$('.switch-checkbox').hasClass('selected')){
+            Common.alertBox.add('请勾选条款及条件');
+            validate = false;
+        };
 
         if(validate){
             return true;
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     inject = require('gulp-inject'),
     sass = require('gulp-sass'),
+    del = require('del'),
     rename = require("gulp-rename"),
     browserSync = require('browser-sync').create();
 
@@ -15,8 +16,16 @@ var path = {
     css:['./app/css/*.css'],
     js:['./app/js/*.js','!app/js/widget.js'],
     dev_js:['./app/dev-js/*.js','./app/dev-js/*/*.js'],
-    index_include_js:['./app/js/lib/zepto.min.js','./app/js/lib/pre-loader.js','./app/js/lib/shake.js','./app/js/rem.js','./app/js/common.js','./app/js/api.js','./app/js/controller.js'],
 };
+
+
+// Not all tasks need to use streams
+// A gulpfile is just another node program and you can use any package available on npm
+gulp.task('clean', function() {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    return del(['build']);
+});
+
 
 // Browser-sync
 gulp.task('browser-sync', function() {
@@ -29,7 +38,7 @@ gulp.task('browser-sync', function() {
 });
 
 //压缩css
-gulp.task('css',function () {
+gulp.task('css',['clean'],function () {
     // 1. 找到文件
     gulp.src(path.css)
         //.pipe(concat('style.css'))
@@ -40,7 +49,7 @@ gulp.task('css',function () {
 });
 
 //uglify all js
-gulp.task('js', function () {
+gulp.task('js', ['clean'],function () {
     // 1. 找到文件
     gulp.src(path.dev_js)
         .pipe(uglify())
@@ -48,34 +57,10 @@ gulp.task('js', function () {
         .pipe(gulp.dest('./app/js/'));
 });
 
-
-//concat and uglify indexjs
-gulp.task('indexjs', function () {
-    // 1. 找到文件
-    gulp.src(path.index_include_js)
-        .pipe(concat('widget_index.js'))
-        // 2. 压缩文件
-        .pipe(uglify())
-        .pipe(rename('widget_index.js'))
-        // 3. 另存为压缩文件
-        .pipe(gulp.dest('./app/js/widget'));
-});
-
-
-//generate index.tpl.php
-gulp.task('generate_index',['css','indexjs'], function () {
-    var target = gulp.src('./app/template/index.html');
-    // It's not necessary to read the files (will speed up things), we're only after their paths:
-    var sources = gulp.src(['./app/js/widget/widget_index.js', './app/css/style.css'], {read: false});
-
-    return target.pipe(inject(sources))
-        .pipe(rename('index.tpl.php'))
-        .pipe(gulp.dest('./template'));
-});
-
 //watch the template
-gulp.task('watch',function(){
+gulp.task('watch',['clean'],function(){
     gulp.watch(path.dev_js,['js']);
+    gulp.watch(path.css,['css']);
 });
 
 //gulp default
